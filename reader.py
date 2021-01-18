40f9800dae9186175ea7c30640345c2773e11596
Shinto C V <cshintov@gmail.com>
2021-01-19 00:27:34 +0530

    Add option to read the articles
    Make this a commandline utility using Click

--- reader.py
+++ reader.py
@@ -1,7 +1,9 @@
 """ RSS feed reader """
+import sys
+import click
 import feedparser as fp
 
-def read(feed):
+def parse(feed):
     rss = fp.parse(feed)
     return (rss, rss.feed.title, rss.feed.subtitle)
 
@@ -13,12 +15,39 @@ def entries(feed):
             details.append(entry.get(item, ''))
         yield details
 
-if __name__ == '__main__':
-    devto = 'https://dev.to/feed/'
-    devto_old = './data/devto.main.feed'
-    pgessays = './data/pgessays.rss'
+def read_feed(url, read):
+    feed, title, desc = parse(url)
 
-    feed, title, desc = read(devto_old)
-    print(f'Count = {len(feed.entries)}')
     for title, description, link in entries(feed):
-        print(f'{title = } \n{description = } \n{link = }\n')
+        if read:
+            wanto_read = input(f'Want to read {title}? [y/n]: ')
+            if wanto_read in ['y', 'Y']:
+                click.launch(link)
+                click.pause(info='Press any key to continue ...')
+            elif wanto_read == 'q':
+                return
+                print('You must be tired! Enjoy your break! And come back soon for more!')
+                sys.exit(0)
+        else:
+            print(f'{title = } \n{description = } \n{link = }\n')
+
+@click.command()
+@click.option('--url', help='Feed url')
+@click.option('--feeds', help='File containing rss feed urls.')
+@click.option('--read/--no-read', default=False, help='Set if you want read the articles.')
+def main(url, feeds, read):
+    """ Parse single or multiple rss feeds """
+    urls = []
+
+    if url:
+        urls = [url]
+    elif feeds:
+        urls = [url for url in open(feeds)]
+    else:
+        print(main.get_help(click.Context(main)))
+
+    for url in urls:
+        read_feed(url, read)
+
+if __name__ == '__main__':
+    main()
--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 # Feeder
 
-A tool to get your favorite rss feeds in your terminal.
+A tool to get your favorite rss feeds in your terminal. Or, if you choose you can easily 
+view the articles in the browser one by one. Helps you to focus!
+
+## Usage
+<pre>
+Browse single or multiple rss feeds
+
+Options:
+  --feeds            File containing rss feeds.
+  --url              An rss feed url.
+  --read /--no-read  Set if you want to read the articles.
+  --help             Show this message and exit.
+</pre>
